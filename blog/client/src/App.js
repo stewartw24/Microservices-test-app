2ceb358341ce6d23fbf80fddeb33a42e6e84a924
William <stewart-w24@hotmail.co.uk>
2020-11-15 16:34:48 +0000

    post submission functionality added

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import PostCreate from './PostCreate';
 
 // eslint-disable-next-line
 export default () => {
-    return <div>Blog app</div>;
+    return (
+        <div className="container">
+            <h1>Create Post</h1>
+            <PostCreate />
+        </div>
+    );
 };
